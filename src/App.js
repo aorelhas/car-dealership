4c741d18d24f4e0d517f825060aa92bd98a20dfb
Carlos Orelhas <c.orelhas@gmail.com>
2020-08-11 21:20:06 +0100

    Structure completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import Cars from './pages/Cars';
+import SingleCar from './pages/SingleCar';
+import Error from './pages/Error';
+
 import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
 import App from './App';
+import { RoomProvider } from './Context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
+  document.getElementById('root')
+);
